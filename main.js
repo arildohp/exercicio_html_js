1eeb9ed73918dbeec85b2efee1e3cb71d823d8f1
arildo horacio pereira <arildohp@gmail.com>
2023-02-17 10:25:49 -0300

    comit inicial

---
+++ main.js
@@ -0,0 +1,17 @@
+function  verificar() {
+    let n1 = document.getElementById('numero-a')
+    let n2 = document.getElementById('numero-b')
+    let valor_a = Number(n1.value)
+    let valor_b = Number(n2.value)
+    let res = document.getElementById('res')
+     if (' valor_a < valor_b')  {
+    windows.alert('valido')
+        } else {
+    windows.alert('invalido')
+    res.innerHTML= `obrigado`
+ }
+ 
+ }
+ 
+
+
